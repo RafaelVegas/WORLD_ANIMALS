6634d8c2efd6ec4e5da4b747e9227b8b79cd9f58
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-03-25 00:12:34 +0100

    Actualizar Raza.java

--- animals/src/main/java/com/world/animals/entities/Raza.java
+++ animals/src/main/java/com/world/animals/entities/Raza.java
@@ -1,5 +1,28 @@
 package com.world.animals.entities;
 
-public class Raza {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="RAZA")
+public class Raza {
+	
+	@Id
+	@Column(name="IDRAZA")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idRaza;
+	
+	@Column
+	private String nombre;
+	
+	private String color;
+	
+	private String caracteristica;
+	
+	private String peso;	
+	
 }
