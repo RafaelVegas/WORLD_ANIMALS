68c1cb0c5e7e3d756176416ff90a2e06127aa460
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-03-26 23:22:18 +0100

    Actualizar AnimalRepository.java, BreedRepository.java y SpeciesRepository.java Se añade la etiqueta de Repository a las clases

--- src/main/java/com/world/animals/repositories/AnimalRepository.java
+++ src/main/java/com/world/animals/repositories/AnimalRepository.java
@@ -3,9 +3,11 @@ package com.world.animals.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.world.animals.entities.Animal;
 
+@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long>{
 
 	Optional<Animal> getAnimalById(Long idAnimal);
--- src/main/java/com/world/animals/repositories/SpeciesRepository.java
+++ src/main/java/com/world/animals/repositories/SpeciesRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.world.animals.entities.Species;
 
+@Repository
 public interface SpeciesRepository extends JpaRepository<Species, String>{
 
 	Optional<Species> getSpeciesByName(String nameSpecies);
