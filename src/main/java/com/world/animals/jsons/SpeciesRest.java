f2229eaf7ee1ba025cf82dbd5a0d1ca8b8866510
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-03-26 23:39:14 +0100

    Actualizar SpeciesRest.java Se crea el Json para traer una especie

---
+++ src/main/java/com/world/animals/jsons/SpeciesRest.java
@@ -0,0 +1,31 @@
+package com.world.animals.jsons;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SpeciesRest {
+
+	@JsonProperty("nombre")
+	private String nombre;
+	
+	@JsonProperty("caracteristica")
+	private String caracteristica;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCaracteristica() {
+		return caracteristica;
+	}
+
+	public void setCaracteristica(String caracteristica) {
+		this.caracteristica = caracteristica;
+	}
+	
+}
