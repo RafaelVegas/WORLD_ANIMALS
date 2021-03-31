ec310036962ecfe962e222ca4655dfd6995784e2
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-03-31 23:54:40 +0200

    Actualizar ContinentService.java y ContinentServiceImpl.java Se crea el servicio

--- src/main/java/com/world/animals/services/ContinentService.java
+++ src/main/java/com/world/animals/services/ContinentService.java
@@ -1,12 +1,16 @@
 package com.world.animals.services;
 
+import java.util.List;
+
 import com.world.animals.exceptions.WorldAnimalException;
 import com.world.animals.jsons.OneContinentAndCountriesRest;
-import com.world.animals.jsons.OneContinentRest;
+import com.world.animals.jsons.ContinentRest;
 
 public interface ContinentService {
 
-	OneContinentRest getContinentByID(Long idContinent) throws WorldAnimalException;
+	ContinentRest getContinentByID(Long idContinent) throws WorldAnimalException;
 	
 	OneContinentAndCountriesRest getContinentAndCountries(Long idContinent) throws WorldAnimalException;
+	
+	List<ContinentRest> getAllContinent() throws WorldAnimalException;
 }
--- src/main/java/com/world/animals/services/impl/ContinentServiceImpl.java
+++ src/main/java/com/world/animals/services/impl/ContinentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.world.animals.services.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,36 +11,44 @@ import com.world.animals.entities.Continent;
 import com.world.animals.exceptions.NotFoundException;
 import com.world.animals.exceptions.WorldAnimalException;
 import com.world.animals.jsons.OneContinentAndCountriesRest;
-import com.world.animals.jsons.OneContinentRest;
+import com.world.animals.jsons.ContinentRest;
 import com.world.animals.repositories.ContinentRepository;
 import com.world.animals.services.ContinentService;
 
 @Service
-public class ContinentServiceImpl implements ContinentService{
+public class ContinentServiceImpl implements ContinentService {
 
 	@Autowired
 	ContinentRepository continentRepository;
-	
+
 	public static final ModelMapper modelmapper = new ModelMapper();
-	
-	//one continent
-	public OneContinentRest getContinentByID(Long idContinent) throws WorldAnimalException {
-		return modelmapper.map(getContinentForMap(idContinent), OneContinentRest.class);
+
+	// one continent
+	public ContinentRest getContinentByID(Long idContinent) throws WorldAnimalException {
+		return modelmapper.map(getContinentForMap(idContinent), ContinentRest.class);
 	}
 
 	private Continent getContinentForMap(Long idContinent) throws WorldAnimalException {
-		return continentRepository.findByIdContinent(idContinent)
-				.orElseThrow(() -> new NotFoundException("NOT FOUND 404", "No se ha encomtrado continent con ID: " + idContinent));
+		return continentRepository.findByIdContinent(idContinent).orElseThrow(
+				() -> new NotFoundException("NOT FOUND 404", "No se ha encomtrado continent con ID: " + idContinent));
 	}
 
-	//one continent and countries
+	// continent and its countries
 	public OneContinentAndCountriesRest getContinentAndCountries(Long idContinent) throws WorldAnimalException {
 		return modelmapper.map(getContinentAndCountriesForMap(idContinent), OneContinentAndCountriesRest.class);
 	}
 
 	private Continent getContinentAndCountriesForMap(Long idContinent) throws NotFoundException {
-		return continentRepository.findByIdContinent(idContinent)
-				.orElseThrow(() -> new NotFoundException("NOT FOUND 404", "No se ha encomtrado continent con ID: " + idContinent));
+		return continentRepository.findByIdContinent(idContinent).orElseThrow(
+				() -> new NotFoundException("NOT FOUND 404", "No se ha encomtrado continent con ID: " + idContinent));
+	}
+
+	// all continents in databases
+	public List<ContinentRest> getAllContinent() throws WorldAnimalException {
+		final List<Continent> continent = continentRepository.findAll();
+		return continent.stream().map(cont -> modelmapper
+								 .map(cont, ContinentRest.class))
+								 .collect(Collectors.toList());
 	}
 
 }
