475c288f0cc53c2ef0c43089ce873b7affc1ed8a
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-04-02 17:32:15 +0200

    Actualizar AnimalService.java Se crea un servicio de animal para obetener su especie y sus razas en BBDD

--- src/main/java/com/world/animals/services/AnimalService.java
+++ src/main/java/com/world/animals/services/AnimalService.java
@@ -1,18 +1,12 @@
 package com.world.animals.services;
 
-import java.util.Set;
-
-import com.world.animals.entities.Continent;
 import com.world.animals.exceptions.WorldAnimalException;
 import com.world.animals.jsons.AnimalRest;
-import com.world.animals.jsons.AnimalSpecieRest;
-import com.world.animals.jsons.AnimalsContinentRest;
+import com.world.animals.jsons.AnimalSpecieBreedRest;
 
 public interface AnimalService {
 
 	AnimalRest getAnimalById(Long id_animal) throws WorldAnimalException;
 	
-	AnimalsContinentRest getAnimalsOfContinent(Set<Continent> rContinent) throws WorldAnimalException;
-	
-	AnimalSpecieRest getAnimalSpecieByIdAnimal(Long id) throws WorldAnimalException;
+	AnimalSpecieBreedRest getAnimalSpecieByIdAnimal(Long id) throws WorldAnimalException;
 }
