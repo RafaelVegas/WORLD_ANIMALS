786721938bc0173531f46c622e4fc11441dde978
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-04-02 17:31:16 +0200

    Actualizar AnimalRepository.java Se actualiza el repository de animal y se utiliza un método para obtener todos los datos de un animal

--- src/main/java/com/world/animals/repositories/AnimalRepository.java
+++ src/main/java/com/world/animals/repositories/AnimalRepository.java
@@ -3,6 +3,7 @@ package com.world.animals.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.world.animals.entities.Animal;
@@ -11,4 +12,7 @@ import com.world.animals.entities.Animal;
 public interface AnimalRepository extends JpaRepository<Animal, Long>{
 
 	Optional<Animal> findByIdAnimal(Long idAnimal);
+	
+	@Query("SELECT ANI FROM Animal ANI WHERE ID_ANIMAL = ?1")
+	public Optional<Animal> getAnimalSpecieBreeds(Long idAnimal);
 }
