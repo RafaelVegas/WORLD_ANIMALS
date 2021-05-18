4d8affffa2967724bbf43d2f223cf8b62e902362
Rafael-Vegas <rafael.vegas.lanzat@gmail.com>
2021-05-18 21:16:38 +0200

    Refactorizacion de la clase persona

--- src/main/java/com/world/animals/entities/Person.java
+++ src/main/java/com/world/animals/entities/Person.java
@@ -11,6 +11,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "PERSONA")
 public class Person {
@@ -32,45 +37,5 @@ public class Person {
 	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JoinColumn(name = "ID_PAIS", nullable = false, unique = true)
 	private Country country;
-
-	public Long getIdPerson() {
-		return idPerson;
-	}
-
-	public void setIdPerson(Long idPerson) {
-		this.idPerson = idPerson;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getNameComplete() {
-		return nameComplete;
-	}
-
-	public void setNameComplete(String nameComplete) {
-		this.nameComplete = nameComplete;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public Country getCountry() {
-		return country;
-	}
-
-	public void setCountry(Country country) {
-		this.country = country;
-	}
 
 }
